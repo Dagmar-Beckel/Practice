78134d56905fc37a65cdc12ba8866bf20a7d8f72
Dagmar-Beckel <beckeldagmar@gmail.com>
2023-07-17 18:45:27 -0500

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Dagmar Beckel on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
